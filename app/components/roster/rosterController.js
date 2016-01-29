5e9d2b53f1d1bb721db8bba1b8ce1fbcaadabaf7
Zmixon91 <zmixon91@gmail.com>
2016-01-29 15:13:45 -0700

    addPlayer re-added

--- app/components/roster/rosterController.js
+++ app/components/roster/rosterController.js
@@ -1,3 +1,8 @@
 app.controller('RosterController', function() {
     this.test = true;
+    this.myRoster = [];
+    
+    this.addPlayer = function(player) {
+        this.myRoster.push({playerName: player.name, playerPosition: player.position, playerNumber: player.number});
+    };
 });
